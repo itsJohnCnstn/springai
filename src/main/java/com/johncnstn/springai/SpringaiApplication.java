18939502402cc458824ad7ee8f7e00df89304a74
Pavel Khankevich <johncnstn@gmail.com>
2026-01-29 10:22:04 +0100

    [prompts/few-shot-prompting] refactor code. add determenistic chat with low temperature. implement few-shot prompting (#2)

--- src/main/java/com/johncnstn/springai/SpringaiApplication.java
+++ src/main/java/com/johncnstn/springai/SpringaiApplication.java
@@ -1,13 +1,31 @@
 package com.johncnstn.springai;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringaiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringaiApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(SpringaiApplication.class, args);
+    }
+
+    @Bean
+    protected ChatClient chat(ChatClient.Builder chatClient) {
+        return chatClient
+                .build();
+    }
+
+    @Bean
+    protected ChatClient factualChat(ChatClient.Builder factualChatClient) {
+        return factualChatClient
+                .defaultOptions(ChatOptions.builder()
+                        .temperature(0.0d)
+                        .build())
+                .build();
+    }
 
 }
